de061488030c1e48399ad16c5cd06250eb26681b
norbertgovaars-ux <norbertgovaars@gmail.com>
2026-08-05 23:00:07 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,5 @@
-const CACHE='auto-assistent-v1';
+const CACHE='auto-assistent-v3-20260805';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>{
-  const url=new URL(e.request.url);
-  if(url.hostname.includes('opendata.rdw.nl')) return;
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
-});
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))])));
+self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(u.hostname.includes('opendata.rdw.nl'))return;e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
