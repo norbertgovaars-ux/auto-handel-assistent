c316d5c01d9da48040fe1803b20f86b703b3d149
norbertgovaars-ux <norbertgovaars@gmail.com>
2026-08-06 19:31:21 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# auto-handel-assistent
-Auto evalueren
+# Auto Handel Assistent 2.0
+
+Upload alle bestanden en mappen naar de root van de GitHub repository.
+
+Belangrijkste onderdelen:
+- vraagprijs en kilometerstand eerst;
+- RDW;
+- directe inkoopbiedingen;
+- minimaal drie benchmarkauto's;
+- marktwaarde, snelle verkoopwaarde en maximaal bod;
+- herstelkosten, extra's en bodbericht.
--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,5 @@
-const CACHE='auto-assistent-v15-1b-20260806-1854';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE='auto-handel-assistent-2-20260806-1';
+const ASSETS=['./','./index.html','./css/app.css','./js/app.js','./js/utils.js','./js/rdw.js','./js/parser.js','./js/valuation.js','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
-self.addEventListener('fetch',e=>{
- const u=new URL(e.request.url);
- if(u.hostname.includes('opendata.rdw.nl')) return;
- e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{
-   const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;
- }).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
-});
+self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(u.hostname.includes('opendata.rdw.nl'))return;e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
